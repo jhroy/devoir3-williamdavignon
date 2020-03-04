e3d610b7f281ada5f8bf6647b2603bd187b80ae2
Jean-Hugues Roy <jh@jhroy.ca>
2020-03-04 01:26:44 -0500

    Moissonnage corrigé
    Tous les scripts [accessibles ici](https://github.com/Journalisme-UQAM/moissonage2020)

--- input2-JHR.py
+++ input2-JHR.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*- 
 import csv, json, encodings
 
-
 dep = input("De quel député voulez-vous savoir le signe astrologique? ")
 dep2 = dep.replace("\xa0", " ")
 # print(dep)
 
-fichier = "New-JHR.csv"
+fichier = "New.csv" ### À QUOI SERT CE FICHIER?
 
-
-with open("signe.csv") as file:
+with open("signe-JHR.csv") as file:
     csv_file = csv.DictReader(file)
     for row in csv_file:
         # nom = str(row["Nom"]).replace(u'\xa0',u ' ').encode('utf-8')
